42bc0b26e78b1e8b0e56632ec46776d825b3087d
j- <j@skeoh.com>
2016-06-16 13:52:24 +1000

    Add prop types to output component

--- src/components/output/index.jsx
+++ src/components/output/index.jsx
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import arrayProduct from '../../utils/array-product';
 
 const tableClassList = 'table table-condensed table-bordered table-highlight';
 
-export default ({ lines }) => {
-	const values = lines.map(line => line.values);
-	const labels = lines.map(line => line.label);
-	const combinations = arrayProduct(values);
-	const hasLabels = lines.some(line => line.label);
-	const hasValues = combinations.length > 0;
+export default class Output extends Component {
+	static propTypes = {
+		lines: PropTypes.array.isRequired,
+	}
 
-	const thead = hasLabels && <thead>
-		<tr>
-			{ labels.map((label, i) =>
-				<th key={ i }>{ label }</th>
-			) }
-		</tr>
-	</thead>;
+	render () {
+		const { lines } = this.props;
+		const values = lines.map(line => line.values);
+		const labels = lines.map(line => line.label);
+		const combinations = arrayProduct(values);
+		const hasLabels = lines.some(line => line.label);
+		const hasValues = combinations.length > 0;
 
-	const tbody = hasValues && <tbody>
-		{ combinations.map((values, i) =>
-			<tr key={ i }>
-				{ values.map((value, i) =>
-					<td key={ i }>{ value }</td>
+		const thead = hasLabels && <thead>
+			<tr>
+				{ labels.map((label, i) =>
+					<th key={ i }>{ label }</th>
 				) }
 			</tr>
-		) }
-	</tbody>;
+		</thead>;
 
-	const table = hasValues && <table className={ tableClassList }>
-		{ thead }
-		{ tbody }
-	</table>;
+		const tbody = hasValues && <tbody>
+			{ combinations.map((values, i) =>
+				<tr key={ i }>
+					{ values.map((value, i) =>
+						<td key={ i }>{ value }</td>
+					) }
+				</tr>
+			) }
+		</tbody>;
 
-	return table;
-};
+		const table = hasValues && <table className={ tableClassList }>
+			{ thead }
+			{ tbody }
+		</table>;
+
+		return table;
+	}
+}
