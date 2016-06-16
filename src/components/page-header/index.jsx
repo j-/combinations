c8408ca5c2b698b6efcb589c4e7b62f3f1f5be5b
j- <j@skeoh.com>
2016-06-16 14:28:16 +1000

    Link to project page from app header

--- src/components/page-header/index.jsx
+++ src/components/page-header/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+const HEADER_URL = 'https://github.com/j-/combinations';
+const HEADER_TITLE = 'Check out the source on GitHub';
+
 export default () => (
 	<div className="page-header">
-		<h1 className="title">Combinations</h1>
+		<h1 className="title">
+			<a href={ HEADER_URL } title={ HEADER_TITLE }>
+				Combinations
+			</a>
+		</h1>
 	</div>
 );
