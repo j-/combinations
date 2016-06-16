94807dd094831f22b99e81589f2c25da2cf837c9
j- <j@skeoh.com>
2016-06-16 15:17:09 +1000

    Convert output component back into a stateless component

--- src/components/output/index.jsx
+++ src/components/output/index.jsx
@@ -1,52 +1,51 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import arrayProduct from '../../utils/array-product';
 
 const tableClassList = 'table table-condensed table-bordered table-highlight';
 
-export default class Output extends Component {
-	static propTypes = {
-		lines: PropTypes.arrayOf(PropTypes.shape({
-			label: PropTypes.string,
-			values: PropTypes.arrayOf(PropTypes.string).isRequired,
-		})).isRequired,
+const Output = ({ lines }) => {
+	const values = lines.map(line => line.values);
+	const combinations = arrayProduct(values);
+	const hasValues = combinations.length > 0;
+
+	if (!hasValues) {
+		return null;
 	}
 
-	render () {
-		const { lines } = this.props;
-		const values = lines.map(line => line.values);
-		const combinations = arrayProduct(values);
-		const hasValues = combinations.length > 0;
+	const labels = lines.map(line => line.label);
+	const hasLabels = lines.some(line => line.label);
 
-		if (!hasValues) {
-			return null;
-		}
+	const thead = hasLabels && <thead>
+		<tr>
+			{ labels.map((label, i) =>
+				<th key={ i }>{ label }</th>
+			) }
+		</tr>
+	</thead>;
 
-		const labels = lines.map(line => line.label);
-		const hasLabels = lines.some(line => line.label);
-
-		const thead = hasLabels && <thead>
-			<tr>
-				{ labels.map((label, i) =>
-					<th key={ i }>{ label }</th>
+	const tbody = <tbody>
+		{ combinations.map((values, i) =>
+			<tr key={ i }>
+				{ values.map((value, i) =>
+					<td key={ i }>{ value }</td>
 				) }
 			</tr>
-		</thead>;
+		) }
+	</tbody>;
 
-		const tbody = <tbody>
-			{ combinations.map((values, i) =>
-				<tr key={ i }>
-					{ values.map((value, i) =>
-						<td key={ i }>{ value }</td>
-					) }
-				</tr>
-			) }
-		</tbody>;
+	const table = <table className={ tableClassList }>
+		{ thead }
+		{ tbody }
+	</table>;
 
-		const table = <table className={ tableClassList }>
-			{ thead }
-			{ tbody }
-		</table>;
-
-		return table;
-	}
+	return table;
 }
+
+Output.propTypes = {
+	lines: PropTypes.arrayOf(PropTypes.shape({
+		label: PropTypes.string,
+		values: PropTypes.arrayOf(PropTypes.string).isRequired,
+	})).isRequired,
+};
+
+export default Output;
