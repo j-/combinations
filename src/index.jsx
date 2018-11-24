e1acc2e9d72a78d164114a3b0f8ac9a40099a834
j- <j@skeoh.com>
2018-11-24 16:58:05 +1100

    Remove why-did-you-update

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,7 @@ import { Provider } from 'react-redux';
 import App from './components/app';
 import createStore from './create-store';
 import { loadState, saveState } from './local-storage';
-import { whyDidYouUpdate } from 'why-did-you-update';
 import 'bootstrap/dist/css/bootstrap.css';
-
-if (__DEV__) {
-	whyDidYouUpdate(React);
-}
 
 const store = window.store = createStore(loadState());
 
