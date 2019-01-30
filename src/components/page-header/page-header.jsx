3aa09d4c5e7373585abad6a9c85b49498c9343af
j- <j@skeoh.com>
2019-01-30 13:45:31 +1100

    Add link to skeoh.com

--- src/components/page-header/page-header.jsx
+++ src/components/page-header/page-header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const PageHeader = () => (
 	<div className="page-header">
+		<a href="https://skeoh.com/" className="text-muted">&larr; skeoh.com</a>
 		<h1 className="title mt-5 mb-5">
 			Combinations
 		</h1>
