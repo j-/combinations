ecb42d6996396c1d1591398ae31766624783905f
j- <j@skeoh.com>
2019-01-30 13:48:09 +1100

    Remove top margin from heading

--- src/components/page-header/page-header.jsx
+++ src/components/page-header/page-header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PageHeader = () => (
 	<div className="page-header">
 		<a href="https://skeoh.com/" className="text-muted">&larr; skeoh.com</a>
-		<h1 className="title mt-5 mb-5">
+		<h1 className="title mb-5">
 			Combinations
 		</h1>
 	</div>
